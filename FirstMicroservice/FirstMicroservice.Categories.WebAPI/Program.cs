b21648baedb0987e7f38f191e0ca8262ac868192
Emre Tümer <emreetumer0@gmail.com>
2025-11-11 10:51:48 +0300

    categories project created

---
+++ FirstMicroservice/FirstMicroservice.Categories.WebAPI/Program.cs
@@ -0,0 +1,42 @@
+using FirstMicroservice.Categories.WebAPI.Context;
+using FirstMicroservice.Categories.WebAPI.Dtos;
+using FirstMicroservice.Categories.WebAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer"));
+});
+
+var app = builder.Build();
+
+app.MapGet("/categories/getall", async (ApplicationDbContext context, CancellationToken cancellationToken) =>
+{
+    var categories = await context.Categories.ToListAsync(cancellationToken);
+
+    return categories;
+});
+
+app.MapPost("/categories/create", async (CreateCagetoryDto request, ApplicationDbContext context, CancellationToken cancellationToken) =>
+{
+    bool isNameExists = await context.Categories.AnyAsync(c => c.Name == request.Name, cancellationToken);
+
+    if (isNameExists)
+    {
+        return Results.BadRequest(new { Message = "Kategori var." });
+    }
+
+    Category category = new()
+    {
+        Name = request.Name,
+    };
+
+    await context.Categories.AddAsync(category);
+    await context.SaveChangesAsync(cancellationToken);
+
+    return Results.Ok(new { Message = "Category baþarýyla oluþturuldu." });
+});
+
+app.Run();
