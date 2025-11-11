f25df3cceb3f7d9c69920d70b9102237195d97f9
Emre Tümer <emreetumer0@gmail.com>
2025-11-11 09:17:56 +0300

    in memory database created.

---
+++ FirstMicroservice/FirstMicroservice.Todos.WEBAPI/Models/Todo.cs
@@ -0,0 +1,7 @@
+﻿namespace FirstMicroservice.Todos.WEBAPI.Models;
+
+public sealed class Todo
+{
+    public int Id { get; set; }
+    public string Work { get; set; } = default!;
+}
--- FirstMicroservice/FirstMicroservice.Todos.WEBAPI/Program.cs
+++ FirstMicroservice/FirstMicroservice.Todos.WEBAPI/Program.cs
@@ -1,6 +1,31 @@
+using FirstMicroservice.Todos.WEBAPI.Context;
+using FirstMicroservice.Todos.WEBAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseInMemoryDatabase("MyDb");
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/todos/create", (string work, ApplicationDbContext context) =>
+{
+    Todo todo = new()
+    {
+        Work = work,
+    };
+    context.Add(todo);
+    context.SaveChanges();
+    return new { Message = "Baþarýyla Oluþtu" };
+});
+
+app.MapGet("/todos/getall", (ApplicationDbContext context) =>
+{
+    var todos = context.Todos.ToList();
+    return todos;
+});
 
 app.Run();
