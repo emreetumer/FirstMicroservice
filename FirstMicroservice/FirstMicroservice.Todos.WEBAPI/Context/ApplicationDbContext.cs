9d958844fcb9ffc3248f2c3166fd867bac589f2f
Emre Tümer <emreetumer0@gmail.com>
2025-11-11 09:17:56 +0300

    in memory database created.

---
+++ FirstMicroservice/FirstMicroservice.Todos.WEBAPI/Context/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using FirstMicroservice.Todos.WEBAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FirstMicroservice.Todos.WEBAPI.Context;
+
+public sealed class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions options) : base(options) { }
+
+    public DbSet<Todo> Todos { get; set; }
+}
